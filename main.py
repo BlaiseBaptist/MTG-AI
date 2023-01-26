8fb6b28a5111df2f6d4588a72f52eed69f349b82
blaise <blaise.baptist@gmail.com>
2023-01-26 15:09:31 -0500

    code

--- main.py
+++ main.py
@@ -19,15 +19,21 @@ import random
 
 def main():
     cards = pd.read_json("AllCards.json", orient='index')
-    legal_training_cards = ((cards.loc[cards['legalities'] != {}])[["text", "convertedManaCost"]]).replace(np.nan, "")
-    #print("card text and cost: ",legal_training_cards.iloc[[4477]])
+    legal_training_cards = ((cards.loc[cards['legalities'] != {}])[["text", "convertedManaCost","types","power","toughness"]]).replace(np.nan, "")
     Tfidf = TfidfVectorizer()
     X_train, X_test = train_test_split(Tfidf.fit_transform(legal_training_cards["text"]))
     Y_train, Y_test = train_test_split(legal_training_cards["convertedManaCost"])
     clf = RidgeClassifier()
     clf.fit(X_train, Y_train)
-    pred = clf.predict(X_test)
-    print("card pred cost")
+    while 0==0:
+        print("\n\n\n")
+        card = cards.iloc[[random.randrange(legal_training_cards.shape[0])]]
+        print("creature stats: ",card['power'].values[0],"/",card['toughness'].values[0],sep='')
+        print("card cost:",card['convertedManaCost'].values[0])
+        print(card['text'].values[0])
+        pred = clf.predict(Tfidf.transform(card['text']))
+        print("card pred cost",pred[0])
+        input("enter to go to next card")
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 if __name__ == '__main__':
     main()
